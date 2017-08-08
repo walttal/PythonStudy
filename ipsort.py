4aac05be3f95b94784208d8a641a508cde7f6dcd
walttal <wallance.hou@gmail.com>
2017-08-08 09:55:11 +0800

    Delete ipsort.py

--- ipsort.py
+++
@@ -1,7 +0,0 @@
-
-
-iplist = ['192.168.2.32','192.168.1.21','192.168.1.3','192.168.2.9','10.1.2.3']
-
-iplist.sort(lambda x,y:cmp("".join([i.rjust(3,'0') for i in x.split('.')]), "".join([i.rjust(3,'0') for i in y.split('.')]))) 
-
-print iplist
